77fbdc503cc0d60eeb7c34addbffef229a585e8e
Bhanu Sharma <chunnugoli567@gmail.com>
2025-11-09 07:13:35 +0530

    Update download_and_extract.py

--- download_and_extract.py
+++ download_and_extract.py
@@ -1,15 +1,12 @@
 from roboflow import Roboflow
 
 rf = Roboflow(api_key="U3dxBFgpBuXshqMT3QR7")
-
 project = rf.workspace("detection-and-segmentation").project("face-mask-vsxay")
-
 version = project.version(2)
-
 dataset = version.download("yolov8")
-
-print("downloaded successfully!")
+print("downloaded successfully....")
 print("Dataset location:", dataset.location)
+
 
 
 
